c8d2b0c199c294567fb9affb4e63c4655a91c3e6
ParisW97 <114314201+ParisW97@users.noreply.github.com>
2023-02-06 11:06:50 -0500

    Time: 48 ms (76.60%), Space: 13.8 MB (69.14%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        conMap = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        answer = 0
+        for i in range(len(s)):
+            if i == len(s) - 1 or conMap[s[i]] >= conMap[s[i+1]]:
+                answer += conMap[s[i]]
+            else:
+                answer -= conMap[s[i]]
+        return answer
